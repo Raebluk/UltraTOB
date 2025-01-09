7d15949b0d5794bc084b80f4a4afb4e37c9d04f1
Raebluk <raebluk@outlook.com>
2025-01-08 19:02:16 -0700

    feat(entity): add `ValueChangeLog` entity

---
+++ src/entities/ValueChangeLog.ts
@@ -0,0 +1,59 @@
+import { Entity, EntityRepositoryType, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core'
+import { EntityRepository } from '@mikro-orm/sqlite'
+
+import { CustomBaseEntity } from './BaseEntity'
+import { Player } from './Player'
+import { User } from './User'
+
+// ===========================================
+// ================= Entity ==================
+// ===========================================
+
+@Entity({ repository: () => ValueChangeLogRepository })
+export class ValueChangeLog extends CustomBaseEntity {
+
+	[EntityRepositoryType]?: ValueChangeLogRepository
+
+	@PrimaryKey({ autoincrement: true })
+	id: number
+
+	@ManyToOne()
+	player!: Player
+
+	@Property()
+	playerDcTag!: string
+
+	@Property()
+	amount!: number
+
+	@Property({ type: 'string' })
+	type!: 'exp' | 'silver' | 'unknown'
+
+	@Property()
+	note: string
+
+	@ManyToOne()
+	moderator: User
+
+}
+
+// ===========================================
+// =========== Custom Repository =============
+// ===========================================
+
+export class ValueChangeLogRepository extends EntityRepository<ValueChangeLog> {
+
+	async insertLog(player: Player, admin: User, amount: number, type: 'exp' | 'silver' | 'unknown', note: string = ''): Promise<ValueChangeLog> {
+		const log = new ValueChangeLog()
+		log.player = player
+		log.playerDcTag = player.dcTag
+		log.moderator = admin
+		log.amount = amount
+		log.type = type
+		log.note = note
+		await this.em.persistAndFlush(log)
+
+		return log
+	}
+
+}
