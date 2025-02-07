607f00977b14ece41296d20f737fc2cda7da8904
Raebluk <raebluk@outlook.com>
2025-02-07 11:53:38 -0800

    feat(db): enable postgres

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,9 +1,10 @@
 import { Options } from '@mikro-orm/core'
 import { EntityGenerator } from '@mikro-orm/entity-generator'
 import { Migrator } from '@mikro-orm/migrations'
+import { PostgreSqlDriver } from '@mikro-orm/postgresql'
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter'
-import { SqliteDriver } from '@mikro-orm/sqlite'
 
+// import { SqliteDriver } from '@mikro-orm/sqlite'
 // eslint-disable-next-line unused-imports/no-unused-imports
 import { env } from '@/env'
 
@@ -30,8 +31,8 @@ const envMikroORMConfig = {
 		/**
 		 * SQLite
 		 */
-		driver: SqliteDriver,
-		dbName: `${databaseConfig.path}db.sqlite`,
+		// driver: SqliteDriver,
+		// dbName: `${databaseConfig.path}db.sqlite`,
 
 		/**
 		 * MongoDB
@@ -42,12 +43,12 @@ const envMikroORMConfig = {
 		/**
 		 * PostgreSQL
 		 */
-		// driver: PostgreSqlDriver,
-		// dbName: env.DATABASE_NAME,
-		// host: env.DATABASE_HOST,
-		// port: Number(env.DATABASE_PORT),
-		// user: env.DATABASE_USER,
-		// password: env.DATABASE_PASSWORD,
+		driver: PostgreSqlDriver,
+		dbName: env.DATABASE_NAME,
+		host: env.DATABASE_HOST,
+		port: Number(env.DATABASE_PORT),
+		user: env.DATABASE_USER,
+		password: env.DATABASE_PASSWORD,
 
 		/**
 		 * MySQL
