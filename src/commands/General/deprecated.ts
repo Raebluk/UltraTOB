3163a912dd13f3903ee43c36b5392b34526adfdb
Raebluk <raebluk@outlook.com>
2025-02-19 13:24:37 -0800

    chore(cmd): add deprecation notice for `quest abandon`

--- src/commands/General/deprecated.ts
+++ src/commands/General/deprecated.ts
@@ -8,6 +8,18 @@ import { Discord, Injectable, Slash, SlashGroup } from '@/decorators'
 })
 export default class DeprecatedCommand {
 
+	@Slash({
+		name: 'abandon',
+		description: '该命令已弃用，请使用/quest drop',
+	})
+	@SlashGroup('quest')
+	async abandon(interaction: CommandInteraction) {
+		return interaction.reply({
+			content: '该命令已弃用，请使用/quest drop',
+			ephemeral: true,
+		})
+	}
+
 	@Slash({ name: 'expmod' })
 	async expmod(interaction: CommandInteraction) {
 		return interaction.reply({
