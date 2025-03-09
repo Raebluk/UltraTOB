0dfdceb7f6a8d652997c31a67e52ef7b229aab64
Raebluk <raebluk@outlook.com>
2025-03-09 11:21:55 +0800

    feat(cmd): replace local config with guild db config in `vmod`

--- src/commands/Admin/expmod.ts
+++ src/commands/Admin/expmod.ts
@@ -1,9 +1,19 @@
 import { Category } from '@discordx/utilities'
 import { ApplicationCommandOptionType, CommandInteraction } from 'discord.js'
 
-import { yzConfig } from '@/configs'
-import { Discord, Injectable, Slash, SlashChoice, SlashOption } from '@/decorators'
-import { Guild, Player, User, ValueChangeLog } from '@/entities'
+import { Discord, Injectable, Slash, SlashOption } from '@/decorators'
+import {
+	Guild,
+	GuildConfigItem,
+	GuildConfigItemRepository,
+	GuildRepository,
+	Player,
+	PlayerRepository,
+	User,
+	UserRepository,
+	ValueChangeLog,
+	ValueChangeLogRepository,
+} from '@/entities'
 import { Guard, UserPermissions } from '@/guards'
 import { Database, Logger } from '@/services'
 import { resolveGuild, resolveUser } from '@/utils/functions'
@@ -13,10 +23,23 @@ import { resolveGuild, resolveUser } from '@/utils/functions'
 @Category('Admin')
 export default class VModCommand {
 
+	private userRepo: UserRepository
+	private playerRepo: PlayerRepository
+	private guildRepo: GuildRepository
+	private valueChangeLogRepo: ValueChangeLogRepository
+	private configRepo: GuildConfigItemRepository
+	private modLogChannel: string[]
+
 	constructor(
 		private db: Database,
 		private logger: Logger
-	) {}
+	) {
+		this.userRepo = this.db.get(User)
+		this.playerRepo = this.db.get(Player)
+		this.guildRepo = this.db.get(Guild)
+		this.valueChangeLogRepo = this.db.get(ValueChangeLog)
+		this.configRepo = this.db.get(GuildConfigItem)
+	}
 
 	@Slash({ name: 'vmod' })
 	@Guard(
@@ -53,11 +76,6 @@ export default class VModCommand {
 		note: string,
 		interaction: CommandInteraction
 	) {
-		const userRepo = this.db.get(User)
-		const playerRepo = this.db.get(Player)
-		const guildRepo = this.db.get(Guild)
-		const valueChangeLogRepo = this.db.get(ValueChangeLog)
-
 		// make sure type is either `exp` or `sliver`
 		if (!['exp', 'silver'].includes(type)) {
 			return interaction.reply({
@@ -67,25 +85,33 @@ export default class VModCommand {
 		}
 
 		const guild = resolveGuild(interaction)
-		const guildEntity = await guildRepo.findOneOrFail({ id: guild?.id })
+		const guildEntity = await this.guildRepo.findOneOrFail({ id: guild?.id })
 
-		const valueUpdated = await playerRepo.updatePlayerValue({ dcTag, guild: guildEntity }, amount, type)
+		const modLogChannelConfig = await this.configRepo.get('missionBroadcastChannel', guildEntity)
+		this.modLogChannel = modLogChannelConfig !== null
+			? (JSON.parse(modLogChannelConfig.value) as string[])
+			: []
+
+		const valueUpdated = await this.playerRepo.updatePlayerValue({ dcTag, guild: guildEntity }, amount, type)
 		if (!valueUpdated) {
 			return interaction.reply({ content: `Player with tag ${dcTag} not found in guild ${guildEntity.id}. Please contact the admins.` })
 		}
 
 		const interactionUser = resolveUser(interaction)
-		const admin = await userRepo.findOneOrFail({ id: interactionUser!.id })
-		const player = await playerRepo.findOne({ dcTag, guild: guildEntity })
+		const admin = await this.userRepo.findOneOrFail({ id: interactionUser!.id })
+		const player = await this.playerRepo.findOne({ dcTag, guild: guildEntity })
 		if (!player) {
 			interaction.reply({
 				content: '该用户近期更改过 discord 用户名，无法直接更改数值，请联系管理员。',
 			})
 		}
 
-		await valueChangeLogRepo.insertLog(player!, admin!, amount, type, note)
+		await this.valueChangeLogRepo.insertLog(player!, admin!, amount, type, note)
 		const infoStr = `<@${interactionUser!.id}> 刚刚更改了 <@${player!.user.id}> 的 ${type} ${amount}`
-		this.logger.log(infoStr, 'info', true, yzConfig.channels.modLogChannel)
+
+		this.modLogChannel.forEach((channelId) => {
+			this.logger.log(infoStr, 'info', true, channelId)
+		})
 
 		return interaction.reply({ content: infoStr })
 	}
